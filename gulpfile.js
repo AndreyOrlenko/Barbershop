bef771af1a08c6b174f4410133e48d210efa283b
AndreyOrlenko <andriiorlenkoo@gmail.com>
2018-06-03 19:29:44 +0300

    Добавлена базовая стилизация всех страниц (text, background, hover, active, focus).

--- gulpfile.js
+++ gulpfile.js
@@ -341,33 +341,52 @@ gulp.task('serve', function () {
 gulp.task('watch', function () {
 
 //---------------------------------------------SCSS-------------------------------
-  watch([path.watchSrc.scss, path.watchSrc.scssBlocks], {usePolling: true} , function (file) {
+  watch([path.watchSrc.scss, path.watchSrc.scssBlocks],
+      {
+        usePolling: true, interval: 50,
+        binaryInterval: 100,
+        alwaysStat: false,
+        depth: 5,
+        awaitWriteFinish: {
+          stabilityThreshold: 1000,
+          pollInterval: 50
+        }
+      }, function (file) {
 
-    if (file.event === 'unlink') {
-      let filePath = file.path;
-      let FilePathSplit = filePath.split('src')[1];
+        if (file.event === 'unlink') {
+          let filePath = file.path;
+          let FilePathSplit = filePath.split('src')[1];
 
-      if (FilePathSplit.substring(1, 5) === 'scss') {
-        let fileCached = filePath.split('.')[0] + '.css'
+          if (FilePathSplit.substring(1, 5) === 'scss') {
+            let fileCached = filePath.split('.')[0] + '.css'
 
-        delete cached.caches.scss[fileCached];
+            delete cached.caches.scss[fileCached];
 
-        let filePathBuild = 'build\\css\\main.css';
-        del.sync(filePathBuild);
+            let filePathBuild = 'build\\css\\main.css';
+            del.sync(filePathBuild);
+            gulp.start('scss:build');
+          }
+          gulp.start('scss:build');
+
+        } else if (file.event === 'error') {
+          notify.onError();
+        }
         gulp.start('scss:build');
-      }
-      gulp.start('scss:build');
-
-    } else if (file.event === 'error') {
-      notify.onError();
-    }
-    gulp.start('scss:build');
-  });
+      });
 
 //---------------------------------------------SCSS-------------------------------
 
 //---------------------------------------------CSS--------------------------------
-  watch(path.watchSrc.css, {usePolling: true}, function (file) {
+  watch(path.watchSrc.css, {
+    usePolling: true, interval: 100,
+    binaryInterval: 300,
+    alwaysStat: false,
+    depth: 5,
+    awaitWriteFinish: {
+      stabilityThreshold: 2000,
+      pollInterval: 100
+    }
+  }, function (file) {
 
     if (file.event === 'unlink') {
       let filePath = file.path;
@@ -389,7 +408,16 @@ gulp.task('watch', function () {
 
 
 //---------------------------------------------JS-------------------------------
-  watch([path.watchSrc.js, path.watchSrc.jsBlocks], {usePolling: true}, function (file) {
+  watch([path.watchSrc.js, path.watchSrc.jsBlocks], {
+    usePolling: true, interval: 100,
+    binaryInterval: 300,
+    alwaysStat: false,
+    depth: 5,
+    awaitWriteFinish: {
+      stabilityThreshold: 2000,
+      pollInterval: 100
+    }
+  }, function (file) {
 
     if (file.event === 'unlink') {
       let filePath = file.path;
@@ -413,7 +441,16 @@ gulp.task('watch', function () {
 
 //Следим за всеми файлами .js кроме main.js как в src так и в build
 
-  watch([path.watchSrc.jsAll, path.watchSrc.jsNotMain], {usePolling: true}, function (file) {
+  watch([path.watchSrc.jsAll, path.watchSrc.jsNotMain], {
+    usePolling: true, interval: 100,
+    binaryInterval: 300,
+    alwaysStat: false,
+    depth: 5,
+    awaitWriteFinish: {
+      stabilityThreshold: 2000,
+      pollInterval: 100
+    }
+  }, function (file) {
 
     if (file.event === 'unlink') {
       delete cached.caches.jsAll[file.path];
@@ -436,7 +473,16 @@ gulp.task('watch', function () {
 //------------------------------------FONTS---------------------------------------
 
 
-  watch(path.watchSrc.fonts, {usePolling: true}, function (file) {
+  watch(path.watchSrc.fonts, {
+    usePolling: true, interval: 100,
+    binaryInterval: 300,
+    alwaysStat: false,
+    depth: 5,
+    awaitWriteFinish: {
+      stabilityThreshold: 2000,
+      pollInterval: 100
+    }
+  }, function (file) {
 
     if (file.event === 'unlink') {
 
@@ -463,7 +509,16 @@ gulp.task('watch', function () {
 //------------------------------------------HTML----------------------------------------
 //Следим за файлами HTML в папке template и в основной директории
 // За папкой template, если произошли изменения пересобираем основные файлы
-  watch(path.watchSrc.htmlTamplate, {usePolling: true}, function (file) {
+  watch(path.watchSrc.htmlTamplate, {
+    usePolling: true, interval: 100,
+    binaryInterval: 300,
+    alwaysStat: false,
+    depth: 5,
+    awaitWriteFinish: {
+      stabilityThreshold: 2000,
+      pollInterval: 100
+    }
+  }, function (file) {
     if (file.event === 'error') {
       notify.onError()
     }
@@ -472,7 +527,16 @@ gulp.task('watch', function () {
 
 
 // За основными файлами, если файл удален, то удаляем его и из build
-  watch(path.watchSrc.html, {usePolling: true}, function (file) {
+  watch(path.watchSrc.html, {
+    usePolling: true, interval: 100,
+    binaryInterval: 300,
+    alwaysStat: false,
+    depth: 5,
+    awaitWriteFinish: {
+      stabilityThreshold: 2000,
+      pollInterval: 100
+    }
+  }, function (file) {
     if (file.event === 'unlink') {
       delete cached.caches.html[file.path];
       let filePath = file.path;
@@ -491,18 +555,29 @@ gulp.task('watch', function () {
 
 
 //---------------------------------IMAGE----------------------------------------------------
-  watch([path.watchSrc.img, path.src.imgSprite], {usePolling: true}, function (file) {
+  watch([path.watchSrc.img, path.src.imgSprite], {
+    usePolling: true, interval: 100,
+    binaryInterval: 300,
+    alwaysStat: false,
+    depth: 5,
+    awaitWriteFinish: {
+      stabilityThreshold: 2000,
+      pollInterval: 100
+    }
+  }, function (file) {
     if (file.event === 'unlink') {
       let filePath = file.path;
       let FilePathSplit = filePath.split('src');
-      if (FilePathSplit[1].substring(1, 7) === 'blocks'){
+      if (FilePathSplit[1].substring(1, 7) === 'blocks') {
         let relativeImgPath = FilePathSplit[0] + 'src\\blocks\\' + file.basename;
         delete cached.caches.image[relativeImgPath];
         let pathInBuildFile = 'build\\img\\' + file.basename;
         del.sync(pathInBuildFile);
         gulp.start('image:build');
       }
+      gulp.start('image:build');
     }
+    gulp.start('image:build');
   });
 //--------------------------------------------IMAGE---------------------------------------
 
